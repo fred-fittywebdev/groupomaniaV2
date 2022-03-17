9aa803885ae26f1694b7c3e4fceaab9ca5bd1cac
fred-fittywebdev <contact@fittywebdev.fr>
2022-03-17 06:43:11 +0100

    Modifiy Business_positions Entity to simplify queries

--- backend/src/entity/business_position.ts
+++ backend/src/entity/business_position.ts
@@ -1,14 +1,6 @@
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 
-export enum Departments {
-    RETAIL = 'retail',
-    MARKETING = 'marketing',
-    IT = 'it',
-    SALES = 'sales',
-    FINANCE = 'finance',
-    RH = 'rh'
-}
 
 @Entity()
 export class BusinessPosition extends BaseEntity {
@@ -16,11 +8,8 @@ export class BusinessPosition extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column({
-        type: 'enum',
-        enum: Departments
-    })
-    type: string
+    @Column()
+    name: string
 
     @OneToMany(() => User, user => user.position)
     positions: User[]
