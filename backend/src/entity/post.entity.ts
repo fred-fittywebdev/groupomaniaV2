38cb71d007ba4ebfa07841d057c0fbb86110906e
fred-fittywebdev <contact@fittywebdev.fr>
2022-04-09 13:07:55 +0200

    Add some routes modifications and some change on some Entities

--- backend/src/entity/post.entity.ts
+++ backend/src/entity/post.entity.ts
@@ -1,45 +1,54 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Comments } from "./comments.entity";
-import { User } from "./user.entity";
-
+import {
+	BaseEntity,
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	OneToMany,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+import { Comments } from './comments.entity';
+import { User } from './user.entity';
 
 @Entity()
 export class Post extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    @Column()
-    title: string
+	@Column()
+	title: string;
 
-    @Column('text')
-    content: string
+	@Column('text')
+	content: string;
 
-    @Column({
-        nullable: true,
-        default: 'img/posts.post1.png'
-    })
-    image: string
+	@Column({
+		nullable: true,
+		default: 'https://source.unsplash.com/random',
+	})
+	image: string;
 
-    @Column({
-        default: 0
-    })
-    like: number
+	@Column({
+		default: 0,
+	})
+	like: number;
 
-    @Column({
-        default: 0
-    })
-    is_reported: number
+	@Column({
+		default: 0,
+	})
+	is_reported: number;
 
-    @CreateDateColumn()
-    posted_at: Date
+	@CreateDateColumn()
+	posted_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date
+	@UpdateDateColumn()
+	updated_at: Date;
 
-    @ManyToOne(() => User, user => user.posts, {onDelete: 'CASCADE'})
-    @JoinColumn({name: 'user_id'})
-    user: User
+	@ManyToOne(() => User, (user) => user.posts, { onDelete: 'CASCADE' })
+	@JoinColumn({ name: 'user_id' })
+	user: User;
 
-    @OneToMany(() => Comments, comment => comment.post)
-    comments: Comments[]
+	@OneToMany(() => Comments, (comment) => comment.post)
+	comments: Comments[];
 }
--- backend/src/entity/user.entity.ts
+++ backend/src/entity/user.entity.ts
@@ -1,60 +1,65 @@
 import { Role } from './role.entity';
-import { type } from "os";
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { type } from 'os';
+import {
+	BaseEntity,
+	Column,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	OneToMany,
+	PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Post } from './post.entity';
 import { Comments } from './comments.entity';
 
 @Entity()
 export class User extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+	@Column()
+	first_name: string;
 
-    @Column()
-    first_name: string
+	@Column()
+	last_name: string;
 
-    @Column()
-    last_name: string
+	@Column({
+		nullable: true,
+		default: 'username',
+	})
+	username: string;
 
-    @Column({
-        nullable: true,
-        default: "username"
-    })
-    username: string
+	@Column({
+		unique: true,
+	})
+	email: string;
 
-    @Column({
-        unique: true
-    })
-    email: string
+	@Column()
+	password: string;
 
-    @Column()
-    password: string
+	@Column({
+		default: true,
+	})
+	is_valid: boolean;
 
-    @Column({
-        default: true
-    })
-    is_valid: boolean
+	@Column({
+		default: 0,
+	})
+	warnings: number;
 
-    @Column({
-        default: 0
-    })
-    warnings: number
+	@Column({
+		nullable: true,
+		default: 'uploads/avatar-defaut.png',
+	})
+	profile_picture: string;
 
-    @Column({
-        nullable: true,
-        default: "img/profil/moi.png"
-    })
-    profile_picture: string
+	@ManyToOne(() => Role) // Ici un utilsateur ne peut avoir qu'un seul role, alors qu'un rôle peut être assigné a plusieurs utilisateurs
+	@JoinColumn({ name: 'role_id' }) // On spécifie comment on veut appeler la colonne
+	role: Role;
 
-    @ManyToOne(() => Role) // Ici un utilsateur ne peut avoir qu'un seul role, alors qu'un rôle peut être assigné a plusieurs utilisateurs
-    @JoinColumn({name: 'role_id'}) // On spécifie comment on veut appeler la colonne
-    role: Role
+	@OneToMany(() => Post, (post) => post.user)
+	posts: Post[];
 
-
-    @OneToMany(() => Post, post => post.user)
-    posts: Post[]
-
-    @OneToMany(() => Comments, comment => comment.user )
-    comments: Comments[]
-
+	@OneToMany(() => Comments, (comment) => comment.user)
+	comments: Comments[];
 }
